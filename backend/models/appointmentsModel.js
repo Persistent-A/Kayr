bcc158b50358c5ad6cb7bdab05a50be2aa896aed
Persistent-A <apurva.mili@gmail.com>
2023-03-04 19:40:49 -0500

    backend construction initialization

---
+++ backend/models/appointmentsModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const appointmentsSchema = mongoose.Schema({
+    appointment_booktime: {
+        type: Date
+    },
+    appointment_attend_time: {
+        type: Date
+    },
+    appointment_end_time:{
+        type: Date
+    },
+    isRejected_appointment:{
+        type: Boolean
+    }
+
+})
+
+module.exports = mongoose.model('appointment', appointmentsSchema)
---
+++ backend/models/patientsModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+const patientsSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Users'
+    },
+    first_name: {
+        type: String,
+        required: [true, 'Please enter your first name']
+    },
+    last_name: {
+        type: String,
+        required: [true, 'Please enter your last name']
+    },
+    date_of_birth: {
+        type: Date,
+        required: [true, 'Please enter your Date of Birth']
+    },
+    gender: {
+        type: String,
+        required: [true, 'Please select your gender']
+    },
+    phone: {
+        type: String,
+        required: [true, 'Please enter your phone']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter your email']
+    },
+    address: {
+        type: String,
+        required: [true, 'Please enter your address']
+    },
+    marital_status: {
+        type: String,
+        required: [true, 'Please enter your address']
+    },
+    past_medical_history: {
+        type: String,
+        required: [true, 'Please enter your medical history']
+    },
+    allergies: {
+        type: String,
+        required: [true, 'Please enter any allergies']
+    }
+},{
+    timeStamps: true
+})
+
+module.exports = mongoose.model('Patients', patientsSchema)
